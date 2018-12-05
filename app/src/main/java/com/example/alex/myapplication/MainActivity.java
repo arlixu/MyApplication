a11c00b40c6e476698c6818c633f1a6d15b35ee0
xuyali <289964653@qq.com>
2018-12-05 19:19:26 +0800

    2页面。

--- app/src/main/java/com/example/alex/myapplication/MainActivity.java
+++ app/src/main/java/com/example/alex/myapplication/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.alex.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -21,23 +24,34 @@ public class MainActivity extends AppCompatActivity {
                     mTextMessage.setText(R.string.title_home);
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                    //TODO 怠速抖动
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                    //TODO about页面
                     return true;
             }
             return false;
         }
     };
 
+    private Button.OnClickListener mOnClickListener
+            = new Button.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent=new Intent(MainActivity.this,RunCollectActivity.class);
+            startActivity(intent);
+        }
+    };
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        Button nextStep= (Button)findViewById(R.id.next_step);
+        nextStep.setOnClickListener(mOnClickListener);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
