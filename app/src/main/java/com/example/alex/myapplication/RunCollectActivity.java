e09521f36c7b366e64b48f2effca90d36cf7f72d
xuyali <289964653@qq.com>
2018-12-07 11:59:42 +0800

    修改。

--- app/src/main/java/com/example/alex/myapplication/RunCollectActivity.java
+++ app/src/main/java/com/example/alex/myapplication/RunCollectActivity.java
@@ -8,22 +8,18 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.alex.myapplication.constants.ButtonConst;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class RunCollectActivity extends AppCompatActivity {
-    private View.OnClickListener rOnClickListener=new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            int id = v.getId();
-            //
-        }
-    };
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_run_collect);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+    //使用map来存按钮状态。
+    private Map buttonStatus=new HashMap<Integer,String>();
+    private void initButton(){
+        buttonStatus.put(R.id.bt_collect_speed_1,ButtonConst.UNCLICK);
+        buttonStatus.put(R.id.bt_collect_speed_2,ButtonConst.UNCLICK);
+        buttonStatus.put(R.id.bt_collect_speed_3,ButtonConst.UNCLICK);
         //Button 123处理的办法。
         //进入时只有1能点。2，3置灰。
         Button collectSpeed1 = findViewById(R.id.bt_collect_speed_1);
@@ -34,10 +30,32 @@ public class RunCollectActivity extends AppCompatActivity {
         collectSpeed2.setEnabled(false);
         collectSpeed3.setBackgroundResource(R.drawable.clr_pressed);
         collectSpeed3.setEnabled(false);
-        //点x采集后。将x置为采集中。enable false ,颜色标黄。采集的百分比写在后面。
-        collectSpeed1.setOnClickListener();
-        //当采集值达到100%后，颜色标灰，将2变绿，enable=true
 
+        collectSpeed1.setOnClickListener(rOnClickListener);
+        collectSpeed2.setOnClickListener(rOnClickListener);
+        collectSpeed3.setOnClickListener(rOnClickListener);
+    }
+    private View.OnClickListener rOnClickListener=new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            int id = v.getId();
+            //如果按钮状态为UNCLICK
+            if(buttonStatus.get(id).equals(ButtonConst.UNCLICK))
+            {
+              ////点采集后。置为采集中。enable true不变， ,颜色标黄。采集的百分比写在后面。
+//                (Button) v.set
+            }
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_run_collect);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        //初始化按钮逻辑
+        initButton();
         //TODO 点击图标时开始分析问题。并跳转到问题分析页。
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
---
+++ app/src/main/java/com/example/alex/myapplication/constants/ButtonConst.java
@@ -0,0 +1,7 @@
+package com.example.alex.myapplication.constants;
+
+public class ButtonConst {
+    public static final String UNCLICK="unclick";
+    public static final String START="start";
+    public static final String STOP="stop";
+}
